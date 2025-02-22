f9de5a984b076f34f69694855173795f7e35351f
Kurniawan Saputra <saputrak299@gmail.com>
2025-02-22 12:39:33 +0700

    Add user and authentication API with validation and database integration

--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
+// albums
 const albums = require("./api/albums");
 const AlbumsService = require("./services/postgres/AlbumsService");
 const AlbumsValidator = require("./validator/albums");
+
+// songs
 const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongsService");
 const SongsValidator = require("./validator/songs");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
 
@@ -24,6 +42,22 @@ const init = async () => {
   });
 
   await server.register([
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
     {
       plugin: albums,
       options: {
